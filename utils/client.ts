057da301e9f4d575881a1c161a289f119ed3c970
Better Futures <betterfuture032@gmail.com>
2023-02-11 17:09:40 -0500

    add react-icons

--- utils/client.ts
+++ utils/client.ts
@@ -1,7 +1,7 @@
 import sanityClient from '@sanity/client';
 
 export const client = sanityClient({
-    projectId: 'eq8zcuph',
+    projectId: 'zxf19i5i',
     dataset: 'production',
     apiVersion: '2022-09-30',
     useCdn: false,
